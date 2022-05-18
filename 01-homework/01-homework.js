8d15bea863cc7a9bd7653407da648cf32cd49a4a
Nik <mchyzhevskyi.mc@gmail.com>
2022-05-18 15:36:36 +0200

    Update Task 1

---
+++ 01-homework/01-homework.js
@@ -0,0 +1,32 @@
+const apple = 15.678, banana = 123.965, orange = 90.2345;
+
+let maxNum = Math.max(apple, banana, orange);
+let minNum = Math.min(apple, banana, orange);
+let sumGoods = apple + banana + orange;
+let sumRoundDown = Math.floor(apple) + Math.floor(banana) + Math.floor(orange);
+let sumRoundHundreds = Math.round((apple+banana+orange)/100)*100;
+let evenNumber = sumRoundDown % 2 === 0;
+let restFrom500 = 500 - sumGoods;
+let avPriceRound = Number.parseFloat(sumGoods/3).toFixed(2);
+let randomDiscount = Math.random()*100;
+let PayAfterRandomDiscountForClient = Number.parseFloat((1-randomDiscount/100)*sumGoods).toFixed(2);
+let NetProfit = Number.parseFloat(PayAfterRandomDiscountForClient - sumGoods/2).toFixed(2)
+
+
+
+let result = `товари apple = 15.678, banana = 123.965, orange = 90.2345
+максимальне число = ${maxNum}
+мінімальне число = ${minNum}
+вартість всіх товарів = ${sumGoods}
+сума цілої частини вартості = ${sumRoundDown}
+суму товарів округлену до сотень = ${sumRoundHundreds}
+чи є сума всіх товарів парним = ${evenNumber}
+Решта з 500 без округлення = ${restFrom500}
+середнє значення цін, округлене до другого знаку = ${avPriceRound}
+випадкову знижку = ${randomDiscount} %
+суму до оплати після знижки = ${PayAfterRandomDiscountForClient}
+чистий прибуток, якщо клієнт заплатив зі знижкою = ${NetProfit}
+`
+
+console.log(result);
+// console.log("I have changed my homework")
