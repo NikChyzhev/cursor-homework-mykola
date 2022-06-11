44deba91651e890d7f7ca85971957c40cab8a71a
Nik <mchyzhevskyi.mc@gmail.com>
2022-06-11 12:54:04 +0200

    ADD/HW9

--- 00-test-homework/00-test-homework.js
+++ 00-test-homework/00-test-homework.js
@@ -1,43 +1,10 @@
-debugger
-
-function generateAnagrams(word) {
-
-    if (word.length < 2) {
-
-        return [word];
-
-    } else {
-
-        // By declaring all variables outside of the loop,
-        // we improve efficiency, avoiding the needless
-        // declarations each time.
-
-        var anagrams = [];
-        var before, focus, after;
-        var shortWord, subAnagrams, newEntry;
-        
-        for (var i = 0; i < word.length; i++) {
-                        
-            before = word.slice(0, i);
-            focus = word[i];
-            after = word.slice(i + 1, word.length + 1);
-            shortWord = before + after;
-            subAnagrams = generateAnagrams(shortWord);
-            
-            for (var j = 0; j < subAnagrams.length; j++){
-                                
-                newEntry = focus + subAnagrams[j];               
-                anagrams.push(newEntry);
-                console.log('anagrams: ', anagrams);
-            }
-
-        }
-
-        return anagrams;
-
-    }
-
+function sayHi() {
+  alert(this.name);
 }
 
-var result = generateAnagrams("win");
-console.log('result: ', result)
+let user = { name: "John" };
+let admin = { name: "Admin" };
+
+// use call to pass different objects as "this"
+sayHi.call( user ); // John
+sayHi.call( admin ); // Admin
---
+++ 09-homework/main.js
@@ -0,0 +1,41 @@
+const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
+
+const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
+
+const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
+
+const countries = ['ukraine', 'latvia', 'litva'];
+const randCountry = countries[Math.floor(Math.random() * countries.length)];
+
+console.log('Random Country is: ', randCountry);
+
+function getMyTaxes(salary) {
+    console.log(`1. My taxes are ${this.tax * salary}`);
+}
+const taxSalary = getMyTaxes.call(eval(randCountry), +prompt(`enter salary`));
+
+function getMiddleTaxes (country) {
+    console.log(`2. Average tax is ${(this.tax * this.middleSalary).toFixed(2) }` )
+}
+const avgTax = getMiddleTaxes.call(eval(randCountry));
+
+function getTotalTaxes (country) {
+    console.log(`3. Total taxes are ${(this.tax * this.middleSalary * this.vacancies).toFixed(2) }` )
+}
+const totalTaxes = getTotalTaxes.call(eval(randCountry));
+
+function getMySalary(country) {
+    const randSalary = (Math.random()*(2000 - 1500) + 1500).toFixed(2);
+    const taxCalc = (randSalary * this.tax).toFixed(2);
+    const objNew = {
+        salary: randSalary,
+        taxes: taxCalc,
+        profit: (randSalary - taxCalc).toFixed(2),
+    };
+    console.log(objNew);
+};
+
+const mySalary = getMySalary.bind(eval(randCountry));
+
+let timerId = setInterval(mySalary, 1000);
+setTimeout(() => clearInterval(timerId), 10000);
