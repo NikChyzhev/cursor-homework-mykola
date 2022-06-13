ab884bf24e9130a48b7a3d8b79d67851796fb581
Nik <mchyzhevskyi.mc@gmail.com>
2022-06-13 22:50:00 +0200

    ADD/FIX/UPD/CORRECT/HW9

--- 09-homework/main.js
+++ 09-homework/main.js
@@ -4,38 +4,44 @@ const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
-const countries = ['ukraine', 'latvia', 'litva'];
-const randCountry = countries[Math.floor(Math.random() * countries.length)];
+const countries = [ukraine, latvia, litva];
+const countryNames = ['Україна', 'Латвія', 'Литва'];
+const randCountryIndex = Math.floor(Math.random() * countries.length);
 
-console.log('Random Country is: ', randCountry);
+console.log('Random Country is: ', countryNames[randCountryIndex]);
 
 function getMyTaxes(salary) {
     console.log(`1. My taxes are ${this.tax * salary}`);
 }
-getMyTaxes.call(eval(randCountry), +prompt(`enter salary`));
+getMyTaxes.call(countries[randCountryIndex], +prompt(`enter salary`));
 
 function getMiddleTaxes (country) {
     console.log(`2. Average tax is ${(this.tax * this.middleSalary).toFixed(2) }` )
 }
-getMiddleTaxes.call(eval(randCountry));
+getMiddleTaxes.call(countries[randCountryIndex]);
 
 function getTotalTaxes (country) {
     console.log(`3. Total taxes are ${(this.tax * this.middleSalary * this.vacancies).toFixed(2) }` )
 }
-getTotalTaxes.call(eval(randCountry));
+getTotalTaxes.call(countries[randCountryIndex]);
 
 function getMySalary(country) {
-    const randSalary = (Math.random()*(2000 - 1500) + 1500).toFixed(2);
-    const taxCalc = (randSalary * this.tax).toFixed(2);
-    const objNew = {
-        salary: randSalary,
-        taxes: taxCalc,
-        profit: (randSalary - taxCalc).toFixed(2),
+    const loggerInfo = () => {
+        const randSalary = (Math.random()*(2000 - 1500) + 1500).toFixed(2);
+        const taxCalc = (randSalary * this.tax).toFixed(2);
+        const objNew = {
+            salary: randSalary,
+            taxes: taxCalc,
+            profit: (randSalary - taxCalc).toFixed(2),
+        };
+        console.log(objNew);
     };
-    console.log(objNew);
+    loggerInfo();
+    
+    let timerId = setInterval(loggerInfo, 10000);
+    setTimeout(() => clearInterval(timerId), 100000);
 };
 
-const mySalary = getMySalary.bind(eval(randCountry));
+const mySalary = getMySalary.call(countries[randCountryIndex]);
 
-let timerId = setInterval(mySalary, 10000);
-setTimeout(() => clearInterval(timerId), 100000);
+
