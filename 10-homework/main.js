3ce928a43c70307b0f9e615abef81494c2178846
Nik <mchyzhevskyi.mc@gmail.com>
2022-06-15 15:56:43 +0200

    ADD/FIX/UPD/CORRECT/HW10

--- 10-homework/main.js
+++ 10-homework/main.js
@@ -27,7 +27,7 @@ class Student {
         }        
     }
 
-    getAverageMark() {
+    getAverageMark(){
         const sum = this.marks.reduce((prevVal, item) => prevVal + item, 0);
         return +(sum/this.marks.length).toFixed(2);
     }
@@ -40,20 +40,6 @@ class Student {
         this.dismised = false; 
     }  
 }
-
-class BudgetStudent extends Student {
-    constructor(university, course, fullName){
-        super(university, course, fullName); 
-        setInterval(this.getScholarship, 30000);    
-    }
-
-    getScholarship(){
-        if(!this.dismised || this.getAverageMark >= 4){
-            console.log(`Ви отримали 1400 грн. стипендії`);
-        }
-    } 
-}
-
 const student1 = new Student('Вищої Школи Психотерапії м.Одеса', 1, 'Остап Бендер');
 console.log(student1.getInfo());
 console.log(student1.marks);
@@ -65,4 +51,17 @@ console.log(`Студент відрахований?: ${student1.dismised}`);
 console.log(`Оцінки після відрахування: ${student1.marks}`);
 student1.recover();
 console.log(`Після відновлення чи cтудент ще відрахований в базі даних?: ${student1.dismised}`);
+
+class BudgetStudent extends Student {
+    constructor(university, course, fullName){
+        super(university, course, fullName);
+        setInterval(() => this.getScholarship(), 3000); 
+    }
+
+    getScholarship(){               
+        if(!this.dismised && this.getAverageMark() >= 4) {
+            console.log(`Ви отримали 1400 грн. стипендії`);
+        }
+    } 
+}
 const student2 = new BudgetStudent('КНЕУ м.Київ', 3, 'Тарас Бульба');
